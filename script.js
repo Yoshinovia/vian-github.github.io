e40ddfc4fbfaa59aac08da3c705ee333742b7b1b
viviandree <viandri.latief@gmail.com>
2024-10-11 11:32:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-document.addEventListener("mousemove", function(e) {
-    var cursor = document.querySelector(".cursor");
-    var x = e.clientX;
-    var y = e.clientY;
-    var dx = x - cursor.offsetLeft;
-    var dy = y - cursor.offsetTop;
-    var angle = Math.atan2(dy, dx);
-    var speed = 0.1;
-    cursor.style.top = (cursor.offsetTop + Math.sin(angle) * speed) + "px";
-    cursor.style.left = (cursor.offsetLeft + Math.cos(angle) * speed) + "px";
-});
-document.addEventListener("mouseleave", function() {
-    var cursor = document.querySelector(".cursor");
-    cursor.style.display = "none";
-});
-document.querySelector("link[rel='stylesheet']").href = "style.css";
 
